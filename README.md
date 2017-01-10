73d9eb07d63033a3fe5aa44f2655be77e89753e5
KostaNau <naumov.k@gmail.com>
2017-01-11 01:13:46 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+
+## Description
+
+The script accept the input path to the text file and output to the console ten most popular words in this text file in order of decreasing frequency.
+
+
+## How to use
+
+Run script with additional argument: ```text_file``` path to text file or just filename if it place in script's directory.
+
+
+### Example
+
+```
+user$ python3 lang_frequency.py text_file.txt
+```
+or
+```
+user$ python3 lang_frequency.py /some_directory/text_file.txt
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,41 @@
+import collections
+import re
+import argparse
+import time
 
 
-def load_data(filepath):
-    pass
+BUFFER_SIZE = 0x1000
+ENCODING = 'utf-8'
+RE_PATTERN = r'[^\W|\d]+'
 
 
-def get_most_frequent_words(text):
-    pass
+def read_file_buffer(file):
+  flow = True
+  while flow:
+    flow = file.read(BUFFER_SIZE)
+    yield flow
+
+
+def load_all_words(input_filepath):
+    all_words_list = []
+    with open(input_filepath, 'r', encoding=ENCODING) as input_file:
+        for line in read_file_buffer(input_file):
+            word_stack = re.findall(RE_PATTERN, line)
+            for word in word_stack:
+                all_words_list.append(word)
+    return all_words_list
+
+
+def get_most_frequent_word(word_list):
+    most_frequent = collections.Counter(word_list).most_common(10)
+    return most_frequent
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description='Most frequen  word_stack')
+    parser.add_argument('text_file', help="path to any text file or filename for current directory")
+    args = parser.parse_args()
+    input_file_path = args.text_file
+
+    word_list = load_all_words(input_file_path)
+    print(get_most_frequent_word(word_list))
